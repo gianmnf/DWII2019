66fcfb4d327c1550f3d8952cd68bda724c28b7cd
gianveloxbr <gianveloxsi@gmail.com>
2019-03-29 09:03:02 -0300

    Aula 29/03 - Final

--- locadora.diurno/src/locadora/diurno/controller/ModeloController.java
+++ locadora.diurno/src/locadora/diurno/controller/ModeloController.java
@@ -1,9 +1,26 @@
 package locadora.diurno.controller;
 
 import javax.enterprise.context.*;
-import locadora.diurno.dal.*;
+import locadora.diurno.dal.entidade.*;
 import javax.inject.*;
 
+@Named
+@RequestScoped
 public class ModeloController {
+	//Referência ao objeto modelo
+	private Modelo modelo;
+	
+	//Ligação da View - BIND
+	public ModeloController() {
+		this.modelo = new Modelo();
+	}
 
+	//Getters & Setters
+	public Modelo getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Modelo modelo) {
+		this.modelo = modelo;
+	}
 }

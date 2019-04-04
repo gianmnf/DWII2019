54cea0f94c5a0f6ef1d3112047e4a4aa61254c63
gianveloxbr <gianveloxsi@gmail.com>
2019-04-04 09:12:27 -0300

    Aula 0404

---
+++ locadora.diurno/src/locadora/diurno/controller/MarcaController.java
@@ -0,0 +1,24 @@
+package locadora.diurno.controller;
+
+import javax.enterprise.context.*;
+import javax.ejb.*;
+
+import locadora.diurno.bll.interfaces.*;
+import locadora.diurno.dal.entidade.*;
+import javax.inject.*;
+
+import java.util.*;
+
+@Named
+@RequestScoped
+public class MarcaController {
+	
+	//Instanciando EJB
+	@EJB
+	private IMarcaEJB marcaEJB;
+	
+	public List<Marca> todas(){
+		return marcaEJB.obterTodos();
+	}
+
+}

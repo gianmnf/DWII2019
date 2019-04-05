fbae41af2f94e246644eb93209ae7c1a9f201f50
gianveloxbr <gianveloxsi@gmail.com>
2019-04-05 09:12:35 -0300

    Aula 05/04

--- locadora.diurno/src/locadora/diurno/controller/ModeloController.java
+++ locadora.diurno/src/locadora/diurno/controller/ModeloController.java
@@ -1,19 +1,52 @@
 package locadora.diurno.controller;
 
-import javax.enterprise.context.*;
+import javax.enterprise.context.*;	
+import javax.ejb.*;
+
+import locadora.diurno.bll.interfaces.*;
 import locadora.diurno.dal.entidade.*;
+
 import javax.inject.*;
+
+import java.util.*;
+import locadora.diurno.bll.util.*;
+import javax.faces.context.*;
+
+import javax.faces.application.*;
 
 @Named
 @RequestScoped
 public class ModeloController {
 
 	private Modelo modelo;
+	//Salvar
+	@EJB
+	private IModeloEJB modeloEJB;
+	
+	@Inject
+	private FacesContext context;
 	
 	public ModeloController() {
 		this.modelo = new Modelo();
 	}
-
+	//Método para salvar
+	public void salvar() {
+		Mensagem msg = modeloEJB.salvar(modelo);
+		
+		if(msg.getStatus() == MensagemStatus.sucesso) { 
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+					msg.getTexto(),null));		
+			//Cria novo objeto
+			this.modelo = new Modelo();
+		}else {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					msg.getTexto(),null));	
+		}
+	}
+	
+	public List<Modelo> todos(){
+		return modeloEJB.obterTodos();
+	}
 	public Modelo getModelo() {
 		return modelo;
 	}
